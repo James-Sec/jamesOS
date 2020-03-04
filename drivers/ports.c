7656e724b687e1bf8a304070e0bc75764ecfe230
joaoandreotti <joao.andreotti@hotmail.com>
2020-03-04 00:43:03 +0000

    adding 32 port access

--- drivers/ports.c
+++ drivers/ports.c
@@ -29,3 +29,19 @@ void port_word_out (uint16_t port, uint16_t data)
 {
   asm("out %%ax, %%dx" : : "a" (data), "d" (port));
 }
+
+// parameter is a 16bit address
+uint32_t port_dword_in (uint16_t port)
+{
+  uint16_t result;
+  __asm__ ("in %%dx, %%eax" : "=a" (result) : "d" (port));
+  return result;
+}
+
+// parameter is the 16bit port address and the data that will
+// be written to
+void port_dword_out (uint16_t port, uint32_t data)
+{
+  // write value in al to port address stored in dx
+  __asm__ __volatile__ ("out %%eax, %%dx" : : "a" (data), "d" (port));
+}
