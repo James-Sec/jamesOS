b1ea26f9a537bab7a5d8445442e456c3ad93e9f5
joaoandreotti <joao.andreotti@hotmail.com>
2021-09-02 18:45:25 -0400

    net_addressess_set (#1)

---
+++ include/l5_interface.h
@@ -0,0 +1,9 @@
+#ifndef L5_INTERFACE_H
+#define L5_INTERFACE_H
+
+#include <stdint.h>
+
+void l5_upper_interface();
+//void l5_lower_interface();
+
+#endif
--- network/udp.c
+++ network/udp.c
@@ -10,7 +10,6 @@ struct udp_segment* build_udp_segment (struct udp_segment *udp, uint16_t source_
   set_bytes_attr_value (udp->header, UDP_CHECKSUM_OFFSET, UDP_CHECKSUM_SIZE, &checksum);
   //checksum = internet_checksum (udp->header, UDP_HEADER_SIZE, data, data_size);
   //set_bytes_attr_value (udp->header, UDP_CHECKSUM_OFFSET, UDP_CHECKSUM_SIZE, &checksum);
-
   memcpy (data, udp->data, data_size);
 }
 
@@ -31,10 +30,12 @@ void recv_udp_segment (uint32_t ip, uint8_t mac[6], uint8_t *data, uint32_t data
   struct udp_segment *segment = kmalloc_u (sizeof (struct udp_segment));
   array_to_udp (segment, data, data_size);
 
-  uint16_t port;
-  get_bytes_attr_value (segment->header, UDP_DESTINATION_PORT_OFFSET, UDP_DESTINATION_PORT_SIZE, &port);
-  ntohs(&port);
-  forward_segment_to_process (port, segment->data, data_size - UDP_HEADER_SIZE);
+  uint16_t destination_port;
+  get_bytes_attr_value (segment->header, UDP_DESTINATION_PORT_OFFSET, UDP_DESTINATION_PORT_SIZE, &destination_port);
+  ntohs(&destination_port);
+  uint16_t source_port;
+  get_bytes_attr_value (segment->header, UDP_SOURCE_PORT_OFFSET, UDP_SOURCE_PORT_SIZE, &source_port);
+  forward_segment_to_process (destination_port, segment->data, data_size - UDP_HEADER_SIZE, source_port, ip, mac);
 
   kfree (segment->data, data_size - UDP_HEADER_SIZE);
   kfree (segment, sizeof (struct udp_segment));
@@ -57,15 +58,18 @@ uint8_t* udp_to_array (struct udp_segment *udp, uint32_t data_size)
   return array;
 }
 
-void forward_segment_to_process (uint16_t port, uint8_t* data, uint32_t data_size)
+void forward_segment_to_process (uint16_t port, uint8_t* data, uint32_t data_size, uint16_t source_port, uint32_t source_ip, int8_t* source_mac)
 {
   if (!udp_port_table [port].pid)
     return;
   memcpy(data, udp_port_table [port].data, data_size);
+  memcpy(source_mac, udp_port_table [port].net_addresses.mac, 6);
+  udp_port_table [port].net_addresses.ip = source_ip;
+  udp_port_table [port].net_addresses.port = source_port;
   soft_unblock_task (udp_port_table [port].pid);
 }
 
-int32_t udp_port_bind (uint16_t port, uint8_t* data)
+int32_t udp_port_bind (uint16_t port, uint8_t* data, struct net_address_set* address)
 {
   if (!port)
   {
@@ -94,7 +98,7 @@ int32_t udp_port_unbind (uint16_t port)
   return 0;
 }
 
-void task_receive_udp (uint8_t* data)
+void task_receive_udp ()
 {
   block_task (BLOCKED);
 }
